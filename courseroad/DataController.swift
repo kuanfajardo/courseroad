8b672185572bf8af197b601b2c4f908d9705f3b2
Juan Diego Fajardo <jfajardo@mit.edu>
2017-02-12 18:38:05 -0500

    C/P DataController.swift

--- courseroad/DataController.swift
+++ courseroad/DataController.swift
@@ -6,4 +6,57 @@
 //  Copyright © 2017 Juan Diego Fajardo. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+import CoreData
+
+class DataController: NSObject {
+    var managedObjectContext: NSManagedObjectContext
+    override init() {
+        // This resource is the same name as your xcdatamodeld contained in your project.
+        guard let modelURL = Bundle.main.url(forResource: "Model", withExtension:"momd") else {
+            fatalError("Error loading model from bundle")
+        }
+        // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Error initializing mom from: \(modelURL)")
+        }
+        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = psc
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            let docURL = urls[urls.endIndex-1]
+            /* The directory the application uses to store the Core Data store file.
+             This code uses a file named "DataModel.sqlite" in the application's documents directory.
+             */
+            let storeURL = docURL.appendingPathComponent("coarseroad.sqlite")
+            do {
+                try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+            } catch {
+                fatalError("Error migrating store: \(error)")
+            }
+        }
+    }
+    
+    func saveContext () -> Bool {
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+                return true
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                //                abort()
+                
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+}
+
