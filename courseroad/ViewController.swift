68a1deb15f411963aeef578509dcce8f336ac823
Juan Diego Fajardo <jfajardo@mit.edu>
2017-02-12 17:02:18 -0500

    Initial Commit

---
+++ courseroad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  courseroad
+//
+//  Created by Juan Diego Fajardo on 2/12/17.
+//  Copyright © 2017 Juan Diego Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
